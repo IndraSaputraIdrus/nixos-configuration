4c36c27c5eaf1f641e87ac0f182cbf6c13cee43b
nix-drz <indrasaputraidrus@gmail.com>
2025-05-24 00:02:58 +0800

    remove home-manager cli

--- configuration.nix
+++ configuration.nix
@@ -16,29 +16,22 @@ in
 	# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
 
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
+	# List packages installed in system profile. To search, run: $ nix search wget
 	environment.systemPackages = with pkgs; [
 		git
-		htop
 		bspwm
 		sxhkd
 		polybar
 		rofi
 		kitty
-		neofetch
 		chromium
 		ntfs3g
-    unzip
 		nodejs_23
     xclip
 
 
     # Unstable packages
 		unstable-pkgs.neovim
-
-    # home manager command
-    inputs.home-manager.packages.${system}.home-manager
 	];
 
   # Home manage
--- home.nix
+++ home.nix
@@ -3,23 +3,14 @@
 let
 in 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "nixdrz";
   home.homeDirectory = "/home/nixdrz";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -36,6 +27,11 @@ in
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+
+    btop
+    neofetch
+    tree
+    unzip
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -53,22 +49,6 @@ in
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/nixdrz/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
 		EDITOR = "nvim";
 		VISUAL = "nvim";
@@ -77,5 +57,10 @@ in
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.bash.enable = true;
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo I use nixos btw";
+    };
+  };
 }
