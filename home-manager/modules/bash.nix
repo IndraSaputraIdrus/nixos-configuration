154c640f17bdf67019d19d485e80f61ca05cd078
nix-drz <indrasaputraidrus@gmail.com>
2025-08-24 23:31:47 +0800

    update bash

--- home-manager/modules/bash.nix
+++ home-manager/modules/bash.nix
@@ -6,11 +6,11 @@
     shellAliases = {
       btw = "echo I use nixos btw";
       nrs = "sudo nixos-rebuild switch --flake";
+      cdproject = "cd $HOME/Documents/projects";
     };
 
     initExtra = ''
-      export PS1='\n\[\e[38;5;202;7m\] \[\e[38;5;202m\][\[\e[38;5;202;1;3m\]\u\[\e[0;7;38;5;202m\]:\[\e[38;5;202m\]\w\[\e[38;5;202m\]]\[\e[0;48;5;202m\] \[\e[0m\] \[\e[38;5;40m\]-\[\e[38;5;40m\]>\[\e[0m\] \\$ '
-      export PATH="$HOME/.local/bin:$PATH"
+      export PS1='\n\[\e[38;5;35m\]\w\[\e[0m\] \\$ '
     '';
   };
 }
